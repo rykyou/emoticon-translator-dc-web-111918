0782f5c0123c21ec364e99a6bee1ddea93f9251c
Robin Kim <rkim1214@gmail.com>
2018-11-06 00:49:11 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,6 +6,7 @@ def load_library(file_path)
   emoticons = YAML.load_file(file_path)
   new_hash = {}
   emoticons.each do |meaning, emoticon_array|
+    if new_hash["get_meaning"] 
     new_hash["get_meaning"] = {emoticon_array[1] => meaning}
     new_hash["get_emoticon"] = {emoticon_array[0] => emoticon_array[1]}
     binding.pry
