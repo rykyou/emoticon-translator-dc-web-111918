f673a55d890cdabf199ba42de04c2c81fc99bf33
Robin Kim <rkim1214@gmail.com>
2018-11-06 01:08:56 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -28,7 +28,7 @@ def get_japanese_emoticon(file_path, emoticon)
   if outcome.length > 0 
     outcome
   else 
-    puts "Sorry, that emoticon was not found"
+    "Sorry, that emoticon was not found"
   end
 end
 
